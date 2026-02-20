3b18a1e4523a46413f88c1453d829dac9496b061
goofy9901 <borissavicc789@gmail.com>
2026-02-20 12:04:55 +0100

    Repeating String

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let result = "";
+  if (num < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < num; i++) {
+    result += string;
+  }
+  return result;
 };
+repeatString("hey", 10);
+repeatString("hey", 1);
+repeatString("hey", 0);
 
 // Do not edit below this line
 module.exports = repeatString;
