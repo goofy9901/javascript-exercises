e48ba35703ee7c142e0957472c00974917011c00
goofy9901 <borissavicc789@gmail.com>
2026-02-21 15:40:26 +0100

    Summing All Integers

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (first, second) {
+  let sum = 0;
+  let start = Math.min(first, second);
+  let end = Math.max(first, second);
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  if (!Number.isInteger(first) || !Number.isInteger(second)) return "ERROR";
+  if (start < 0 || end < 0) {
+    return "ERROR";
+  }
+  if (start % 1 !== 0 || end % 1 !== 0) {
+    return "ERROR";
+  }
 
+  return sum;
 };
 
 // Do not edit below this line
