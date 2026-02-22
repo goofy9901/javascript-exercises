594fb61c92838ec7fb433112cfd1f2604248a436
goofy9901 <borissavicc789@gmail.com>
2026-02-22 14:39:12 +0100

    Converting temperatures

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahT) {
+  let celTemp = Math.round((fahT - 32) * (5 / 9) * 10) / 10;
+  return celTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celT) {
+  let fahTemp = Math.round((celT * (9 / 5) + 32) * 10) / 10;
+  return fahTemp;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
