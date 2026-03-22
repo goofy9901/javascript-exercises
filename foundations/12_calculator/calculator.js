a3dd9fa989fe9acabc4d69acf1e0225e41c7ca57
goofy9901 <borissavicc789@gmail.com>
2026-03-22 19:23:00 +0100

    Objects Calulator Exercises

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  let sum = num1 + num2;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  let sub = num1 - num2;
+  return sub;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sumArr = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sumArr = sumArr + arr[i];
+  }
+  return sumArr;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let mulArr = 1;
+  for (let i = 0; i < arr.length; i++) {
+    mulArr = mulArr * arr[i];
+  }
+  return mulArr;
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  let powArr = 1;
+  for (let i = 0; i < exponent; i++) {
+    powArr = powArr * base;
+  }
+  return powArr;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let factNum = 1;
+  for (let i = n; i >= 1; i--) {
+    factNum = factNum * i;
+  }
+  return factNum;
 };
 
 // Do not edit below this line
@@ -29,5 +47,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
