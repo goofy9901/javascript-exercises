dcf8a02e0bbfcb194531a1a0ef18154c22880cb5
goofy9901 <borissavicc789@gmail.com>
2026-03-23 12:54:48 +0100

    Find The Oldest Person

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  return arr.reduce(function (best, current) {
+    const bestAge =
+      (best.yearOfDeath || new Date().getFullYear()) - best.yearOfBirth;
 
+    const currentAge =
+      (current.yearOfDeath || new Date().getFullYear()) - current.yearOfBirth;
+
+    if (currentAge > bestAge) {
+      return current;
+    } else {
+      return best;
+    }
+  });
 };
 
 // Do not edit below this line
