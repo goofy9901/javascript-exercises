f8409d84327ce7dc131663ae4de46dafc350c086
goofy9901 <borissavicc789@gmail.com>
2026-03-23 11:56:30 +0100

    Fibonacci sequence

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  n = Number(n);
+  if (n < 0) {
+    return "OOPS";
+  }
+  let number1 = 0;
+  let number2 = 1;
 
+  for (let i = 0; i < n; i++) {
+    let currentNum = number1 + number2;
+    number1 = number2;
+    number2 = currentNum;
+  }
+  return number1;
 };
 
 // Do not edit below this line
