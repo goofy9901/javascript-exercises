3ffeda799e15e4320883da421031763b4d15cd86
goofy9901 <borissavicc789@gmail.com>
2026-03-23 11:21:02 +0100

    Palindromes Exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let rev = str
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, "")
+    .split("");
+  let n = rev.length - 1;
+  for (let i = 0; i < rev.length / 2; i++) {
+    if (rev[i] !== rev[n - i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
